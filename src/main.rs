d33c04959a389c1ab153cd1bb3bc42f83f4a5518
jacksonsilver <jacksonsilver@cmail.carleton.ca>
2022-05-05 14:10:30 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ fn validate_grading_system() {
 
 /// Calculate the grade
 fn calculate_grade(carrots: i32, nuts: i32, seeds: i32) -> i32 {
-    return (carrots + nuts) * (carrots + nuts + seeds);
+    (carrots + nuts) * (carrots + nuts + seeds)
 }
 
 /// Validate the safety system
@@ -49,11 +49,7 @@ fn calculate_safety_status(
     has_carrot: bool,
     friends_nearby: i32,
 ) -> bool {
-    if wolves_nearby == false && day_time == true || has_carrot == true || friends_nearby > 3 {
-        return true;
-    } else {
-        return false;
-    }
+    !wolves_nearby && day_time || has_carrot || friends_nearby > 3
 }
 
 /// Validate the simulation
